3b7a07ea618a3f76e1b98b2d9535ada965f914fe
apopova <61460069+AnastasiaTWW@users.noreply.github.com>
2022-05-31 19:01:19 +0500

    temp delete GTM

--- stylesheets/extra.js
+++ stylesheets/extra.js
@@ -235,18 +235,3 @@ function resizeMessagePostToParentIframe(height) {
 
   window.parent.postMessage(message, '*');
 }
-
-// Google Tag Manager
-
-(function(w,d,s,l,i){
-  w[l]=w[l]||[];
-  w[l].push({
-    'gtm.start':new Date().getTime(),event:'gtm.js'
-  });
-  var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-  j.async=true;
-  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-  f.parentNode.insertBefore(j,f);
-})
-(window,document,'script','dataLayer','GTM-P8GHH8M');
-// End Google Tag Manager
