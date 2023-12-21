b4180286e533a794a6b8c178e34c56839ffa3f69
apopova <61460069+AnastasiaTWW@users.noreply.github.com>
2023-12-21 18:39:00 +0400

    Update extra.js

--- stylesheets/extra.js
+++ stylesheets/extra.js
@@ -383,6 +383,11 @@ window.smartlook||(function(d) {
   })(document);
   smartlook('init', '639473b00998b72cb849754253a85d6981296f7d', { region: 'eu' });
 
+// Heap integration
+
+  window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+  heap.load("3569477503");
+
 /*
 * Listen to the main navigation clicks.
 * */
